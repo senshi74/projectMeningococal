6144737c97f569b0ebae4fc2144ccfeb63e3c990
senshi74 <43126227+senshi74@users.noreply.github.com>
2021-06-06 14:05:41 +0800

    script of dialogs for unity games

---
+++ DialogueManager.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour {
+    
+    public Text nametext;
+    public Text dialogueText;
+    public Image dialogueBox;
+
+    public Animator animator;
+    
+
+    private Queue<string> sentences;
+
+	// Use this for initialization
+	void Start ()
+    {
+        sentences = new Queue<string>();
+        nametext.enabled = false;
+        dialogueText.enabled = false;
+        dialogueBox.enabled = false;
+        animator.SetBool("isUse", false);
+        
+
+    }
+
+    public void StartDialogue(Dialogue dialogue, int textDelay)
+    {
+        nametext.text = dialogue.name;
+
+        sentences.Clear();
+
+        nametext.enabled = true;
+        dialogueText.enabled = true;
+        dialogueBox.enabled = true;
+        animator.SetBool("isUse", true);
+        
+
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        //Invoke("EndDialogue", textDelay);
+
+        //DisplayNextSentence();
+
+        string sentencing = sentences.Dequeue();
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(sentencing));
+    }
+
+    public void StopDialogue(int textDelay)
+    {
+        Invoke("EndDialogue", textDelay);
+    }
+
+    public void DisplayNextSentence()
+    {
+        if (sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(sentence));
+    }
+
+    IEnumerator TypeSentence (string sentence)
+    {
+        dialogueText.text = "";
+        foreach (char letter in sentence.ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return null;
+        }
+    }
+    void EndDialogue()
+    {
+        //Debug.Log("End of conversation.");
+        nametext.enabled = false;
+        dialogueText.enabled = false;
+        dialogueBox.enabled = false;
+        animator.SetBool("isUse", false);
+        
+    }
+}
---
+++ DialogueTrigger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour {
+
+    public int textDelay;
+
+    public Dialogue dialogue;
+    /*
+    public void TriggerDialogue()
+    {
+        FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+    }*/
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            FindObjectOfType<DialogueManager>().StartDialogue(dialogue, textDelay);
+        }
+    }
+
+    public void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            FindObjectOfType<DialogueManager>().StopDialogue(textDelay);
+        }
+    }
+}
